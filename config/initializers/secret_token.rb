67af9d0d00374871436e473746a1eea40e021d3a
Peter Barnes <barnespeter213@gmail.com>
2014-04-08 14:24:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameroomApp::Application.config.secret_key_base = 'aca651f97db69a3c8d59e361328fc4d3c6f10db07fc612803f493832a82182268fc85f3fac3779821f3baa17f76b10851e85fcc1a3026f10f97780c60dbe733d'
