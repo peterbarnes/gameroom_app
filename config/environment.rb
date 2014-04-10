df89589c106fe5bac750c83a697e266b1f22606a
Peter Barnes <barnespeter213@gmail.com>
2014-04-10 14:19:14 -0500

    Update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 GameroomApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SG_USERNAME"],
+  :password => ENV["SG_PASSWORD"],
+  :domain => 'gameroom.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
