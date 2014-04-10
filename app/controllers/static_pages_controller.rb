76c93494f847daf84cea91a441a8e73ae2cb8f90
Peter Barnes <barnespeter213@gmail.com>
2014-04-10 14:05:45 -0500

    Update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,17 @@ class StaticPagesController < ApplicationController
 
   def locations
   end
+
+  def index
+    params[:email] ||= 'barnespeter213@gmail.com'
+  end
+
+  def sendit
+    if UserMailer.email(params).deliver
+      flash[:success] = "Your message has been sent"
+    else
+      flash[:error] = "Something went wrong"
+    end
+      redirect_to contact_path
+  end
 end
